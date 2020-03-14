e867f3e1d850bbfa8b9bd830329118929c41772b
RobinL <robinlinacre@hotmail.com>
2020-03-14 17:28:37 +0000

    Deploying to gh-pages from  - be96bf5f4fac6de548243cb3e56b583644faa0da 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/splink_settings_editor/precache-manifest.1a76004604ff3baebefdce52a2bc4f2b.js"
+  "/splink_settings_editor/precache-manifest.f5779c7c11494cc8d776b3d14bf43233.js"
 );
 
 self.addEventListener('message', (event) => {
