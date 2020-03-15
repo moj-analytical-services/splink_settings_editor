2ef32266c785c19708d0218f961e8ce47dbdb1ff
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2020-03-15 18:31:18 +0000

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,7 @@ import React from 'react';
 function Header(props) {
     return <div>
         <h1>splink settings examples and editor</h1>
-    <p>Use the editor below to create a <code>splink</code> settings dictionary, or load one of our examples:</p>
-
-
+    <p>Use the editor below to create a <code>splink</code> settings dictionary or load one of our examples:</p>
     </div>;
   }
 
